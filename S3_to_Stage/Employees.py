aca9a3cb4674c311d13fc3ba06a37256b8287ca5
SureshGajji <suresh.gajji@bizacuity.com>
2024-03-01 13:53:06 +0530

    adding s3 to stage scripts
    added all scripts including Master script to copy data from s3 to reshift tables

---
+++ S3_to_Stage/Employees.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+
+def copy_data_from_s3_to_redshift(bucket_name, file_path, redshift_params):
+    conn = psycopg2.connect(
+        dbname=redshift_params['dbname'],
+        user=redshift_params['user'],
+        password=redshift_params['password'],
+        host=redshift_params['host'],
+        port=redshift_params['port']
+    )
+    cur = conn.cursor()
+    s3_path = f's3://{bucket_name}/{file_path}'
+    sql = f"""
+    COPY {redshift_params['schema']}.{redshift_params['table']}
+    FROM '{s3_path}'
+    IAM_ROLE '{redshift_params['iam_role']}'
+    CSV
+    IGNOREHEADER 1
+    """
+    print(" Employees data loaded")
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+
+    bucket_name = 'etlpython'
+    file_path = 'Employees.csv'
+    redshift_params = {
+        'dbname': 'dev',
+        'user': 'admin',
+        'password': 'Surya183',
+        'host': ('default-workgroup.278921652245.us-east-1.'
+                 'redshift-serverless.amazonaws.com'),
+        'port': '5439',
+        'schema': 'devstage',
+        'table': 'employees',
+        'iam_role': ('arn:aws:iam::278921652245:role/service-role'
+                     '/AmazonRedshift-CommandsAccessRole-20240221T231641')
+    }
+copy_data_from_s3_to_redshift(bucket_name, file_path, redshift_params)
---
+++ S3_to_Stage/Master.py
@@ -0,0 +1,33 @@
+import subprocess
+import psycopg2
+
+conn = psycopg2.connect(
+        dbname='dev',
+        user='admin',
+        password='Surya183',
+        host=('default-workgroup.278921652245.us-east-1.'
+              'redshift-serverless.amazonaws.com'),
+        port='5439'
+    )
+print("Connected to Redshift successfully")
+cursor = conn.cursor()
+cursor.execute("Truncate table devstage.customers;")
+cursor.execute("Truncate table devstage.employees;")
+cursor.execute("Truncate table devstage.offices;")
+cursor.execute("Truncate table devstage.orderdetails;")
+cursor.execute("Truncate table devstage.orders;")
+cursor.execute("Truncate table devstage.payments;")
+cursor.execute("Truncate table devstage.productlines;")
+cursor.execute("Truncate table devstage.products;")
+print("Truncated successfully")
+cursor.close()
+python_files = ['Customers.py', 'Employees.py', 'Offices.py',
+                'OrderDetails.py', 'Orders.py', 'Payments.py',
+                'ProductLines.py', 'Products.py']
+x = "C:/Users/Suresh.gajji/Desktop/ETL Python/ETL/ETL-Python/S3_to_Stage"
+
+for file in python_files:
+    y = x+"/"+file
+    print('path', y)
+    subprocess.call(['python', x+"/"+file])
+print("All tables data loaded successfully")
